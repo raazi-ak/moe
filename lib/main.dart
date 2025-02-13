48d1ee10a23ad88fca3dae2a994c42dd69564e70
Raazi Faisal <raazi.faisal@gmail.com>
2025-02-13 22:07:03 +0530

    feat: added localization and set up done

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,13 +22,22 @@ class MyApp extends StatelessWidget {
         Locale('en'), // English
         Locale('de'), // German
       ],
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context)!.helloWorld),
-        ),
-        body: Center(
-          child: Text(AppLocalizations.of(context)!.helloWorld),
-        ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.helloWorld),
       ),
     );
   }
